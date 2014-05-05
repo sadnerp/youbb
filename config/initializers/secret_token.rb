f484d88447d280ecd8fd685f832badef129d007e
sadnerp <sadnerp@gmail.com>
2014-05-05 23:07:38 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Youbb::Application.config.secret_key_base = 'e3cf19f56711c6452d68bac6c5c0036ea6eacf0b737e8a3c7b6d01c2fe4e6c50c7c3e5ed19c1dbf99fc4a040b8ebceaffed598ff0566a2091c0689b566560a74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Youbb::Application.config.session_store :cookie_store, key: '_youbb_session'
